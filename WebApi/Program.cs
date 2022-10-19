0d51722da2755b6cac233b97a45c6c52520650c9
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2022-10-18 20:51:06 -0400

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,35 @@
+using Serilog;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Host.UseSerilog((ctx, lc) => lc
+                .WriteTo.Console()                
+                .WriteTo.Trace());
+        
+            var app = builder.Build();
+
+            
+            
+            // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
